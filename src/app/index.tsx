a60f5d59c9fc13bb9fd7d1df7a49fadbd74c3f73
Syntacticdev <dsyntacticdev@gmail.com>
2026-09-20 14:19:21 +0100

    Add hero section

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,6 @@
+import Hero from "@/components/home/hero";
 import SafeAreaWrapper from "@/components/ui/safe-area-wrapper";
+import { Search, ShoppingCart } from "lucide-react-native";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
@@ -10,12 +12,23 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
       >
         {/* Header Section */}
-        <View className="">
+        <View className="flex-row items-center justify-between">
           <View className="flex-row items-center">
             <Text className="text-2xl font-semibold">HIGH</Text>
             <Text className="text-2xl font-semibold">Fash</Text>
           </View>
+          <View className="flex-row items-center gap-8">
+            <Search />
+            <View className="relative">
+              <View className="absolute -top-2 -right-4 z-20  h-6 w-6 items-center justify-center rounded-full bg-black">
+                <Text className="text-white text-center">11</Text>
+              </View>
+              <ShoppingCart />
+            </View>
+          </View>
         </View>
+
+        <Hero />
       </ScrollView>
     </SafeAreaWrapper>
   );
---
+++ src/components/home/hero.tsx
@@ -0,0 +1,31 @@
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+
+const hero = () => {
+  return (
+    <View className="bg-[#160e24] relative my-3 h-[300px] w-full rounded-[20px]">
+      <View className="absolute left-5 top-5 flex h-full w-[50%] flex-col items-start justify-center gap-2">
+        <Text className="text-lg font-bold text-slate-200">New Season</Text>
+        <Text className="text-3xl text-white">New Styles</Text>
+        <View className="my-3">
+          <Text className="text-slate-200">Up to 40% off.</Text>
+          <Text className="text-slate-200">Your latest collection</Text>
+        </View>
+
+        <Pressable className="rounded-md py-4 px-6 bg-white text-center ">
+          <Text className="text-[#160e24] text-lg font-bold">Shop Now</Text>
+        </Pressable>
+      </View>
+      <View>
+        <Image
+          resizeMode="contain"
+          source={require("@/assets/images/model.png")}
+          className="h-[300px] w-2/4 rounded-[20px]   self-end   "
+        />
+      </View>
+    </View>
+  );
+};
+
+export default hero;
+
+const styles = StyleSheet.create({});
